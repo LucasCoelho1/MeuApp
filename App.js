c4cf9484bb967a3e05a4f77b632c9b2dd5660065
LucasCoelho1 <lucas.coelho3@alunos.sc.senac.br>
2023-02-27 15:58:34 -0300

    toio

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer, NavigationRouteContext } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { A } from '@expo/html-elements';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Button } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Home" }}/>
+        <Stack.Screen name="Details" component={DetailsScreen} options={{ title: "Details" }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title='Vai pra Detalhes' onPress={() => navigation.navigate('Details')}></Button>
     </View>
   );
 }
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+// SobreScreen
+// Seu nome e um oi e uma frase de impacto!
 
 const styles = StyleSheet.create({
   container: {
